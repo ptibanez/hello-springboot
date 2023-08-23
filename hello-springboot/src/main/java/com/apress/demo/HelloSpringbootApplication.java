091d543be8e4181f9542257b959dcac9c078a1ec
Pedro Tomás Ibáñez <ptibanez@gmail.com>
2023-08-23 16:51:41 +0000

    version incial

--- hello-springboot/src/main/java/com/apress/demo/HelloSpringbootApplication.java
+++ hello-springboot/src/main/java/com/apress/demo/HelloSpringbootApplication.java
@@ -1,4 +1,4 @@
-package com.apress.hellospringboot;
+package com.apress.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ hello-springboot/src/main/java/com/apress/demo/domain/User.java
@@ -0,0 +1,15 @@
+package com.apress.demo.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    
+}
